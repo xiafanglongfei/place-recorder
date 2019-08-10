8deafc9db7676ffd7519b1600b5aed47f4f4d02f
夏龙飞 <xialongfei@xialongfeideMacBook-Air.local>
2019-08-10 15:51:13 +0800

    1.2.5
    新增 公众号和 openid 可以一键设置到剪贴板

--- miniprogram/pages/about/about.js
+++ miniprogram/pages/about/about.js
@@ -14,7 +14,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       openid: app.globalData.openid
     })
@@ -23,49 +23,75 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+
+  setClipboard_gongzhonghao: function(e) {
+    wx.setClipboardData({
+      data: "Aaronsz",
+      success: function (res) {
+        wx.showToast({
+          title: '已复制到剪贴板',
+        })
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  },
+
+  setClipboard_openid: function(e) {
+    wx.setClipboardData({
+      data: this.data.openid,
+      success: function(res) {
+        wx.showToast({
+          title: '已复制到剪贴板',
+        })
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
   }
 })
